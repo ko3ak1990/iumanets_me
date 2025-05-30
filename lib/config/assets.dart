d054058fa632ad452f513f2c88ad8b3b7139e42a
Ievgen Umanets <ko3ak.zhn@gmail.com>
2025-05-30 09:57:42 +0200

    Update resources

--- lib/config/assets.dart
+++ lib/config/assets.dart
@@ -1,15 +1,13 @@
-class Assets{
+class Assets {
   static const moon = 'assets/moon.png';
   static const avatar = 'assets/avatar.webp';
   static const facebook = 'assets/facebook.png';
-  static const github = 'assets/github.png';
   static const instagram = 'assets/instagram.png';
   static const linkedin = 'assets/linkedin.png';
-  static const epam= 'assets/works/epam_logo.png';
+  static const epam = 'assets/works/epam_logo.png';
   static const edsson = 'assets/works/edsson_logo.png';
   static const data_ai = 'assets/works/data_ai_logo.png';
   static const albert_heijn = 'assets/works/albert_heijn_logo.png';
   static const projects = 'assets/projects.png';
   static const aboutMe = 'assets/about.png';
-
 }
--- lib/config/themes.dart
+++ lib/config/themes.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
+
 darkTheme(context) {
   return ThemeData(
     fontFamily: 'GoogleSansRegular',
     primarySwatch: Colors.blue,
     primaryColor: Colors.black,
-    colorScheme: const ColorScheme.dark().copyWith(
-        secondary: Colors.blue,
-        primary: Colors.blue
-    ),
+    colorScheme: const ColorScheme.dark()
+        .copyWith(secondary: Colors.blue, primary: Colors.blue),
     disabledColor: Colors.grey,
     cardColor: const Color(0xff1f2124),
     canvasColor: Colors.black,
@@ -18,21 +17,18 @@ darkTheme(context) {
         backgroundColor: Colors.blue,
       ),
     ),
-    appBarTheme: const AppBarTheme(
-      elevation: 0.0,
-        backgroundColor: Colors.black
-    ),
+    appBarTheme:
+        const AppBarTheme(elevation: 0.0, backgroundColor: Colors.black),
   );
 }
+
 lightTheme(context) {
   return ThemeData(
-     fontFamily: 'GoogleSansRegular',
+    fontFamily: 'GoogleSansRegular',
     primarySwatch: Colors.blue,
     primaryColor: Colors.white,
-    colorScheme:  const ColorScheme.light().copyWith(
-      secondary: Colors.blue,
-        primary: Colors.blue
-    ),
+    colorScheme: const ColorScheme.light()
+        .copyWith(secondary: Colors.blue, primary: Colors.blue),
     disabledColor: Colors.grey,
     textButtonTheme: TextButtonThemeData(
       style: TextButton.styleFrom(
@@ -43,9 +39,7 @@ lightTheme(context) {
     cardColor: Colors.white,
     canvasColor: Colors.white,
     brightness: Brightness.light,
-    appBarTheme: const AppBarTheme(
-      elevation: 0.0,
-      backgroundColor: Colors.white
-    ),
+    appBarTheme:
+        const AppBarTheme(elevation: 0.0, backgroundColor: Colors.white),
   );
 }
